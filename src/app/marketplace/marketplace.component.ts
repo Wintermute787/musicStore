59e0c1043aee03b4b92980cf3b4139e2b392729a
Student <example@example.com>
2019-06-19 14:25:27 -0700

    add firebase and create route

--- src/app/marketplace/marketplace.component.ts
+++ src/app/marketplace/marketplace.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { Album } from '../album.model';
 import { Router } from '@angular/router';
 import { AlbumService } from '../album.service';
+
+import { FirebaseListObservable } from 'angularfire2/database';
  
 @Component({
   selector: 'app-marketplace',
@@ -11,12 +13,12 @@ import { AlbumService } from '../album.service';
 })
 export class MarketplaceComponent implements OnInit {
     constructor(private router: Router, private albumService: AlbumService){};
-  albums: Album[];
-
+ 
+  albums: FirebaseListObservable<any[]>;
   ngOnInit() {
       this.albums = this.albumService.getAlbums();
   }
- goToDetailPage(clickedAlbum: Album){
-     this.router.navigate(['albums', clickedAlbum.id]);
+ goToDetailPage(clickedAlbum){
+    this.router.navigate(['albums', clickedAlbum.$key]);
  }
 }
