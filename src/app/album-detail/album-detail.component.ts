2eb60c9a490da26c405423b39b54f652100ccb9d
Student <example@example.com>
2019-06-19 12:10:40 -0700

    add more files

--- src/app/album-detail/album-detail.component.ts
+++ src/app/album-detail/album-detail.component.ts
@@ -1,23 +1,27 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, Params} from '@angular/router';
+import { ActivatedRoute, Params } from '@angular/router';
 import { Location } from '@angular/common';
-import { Album } from '../album.model'; 
+import { Album } from '../album.model';
+import { AlbumService } from '../album.service';
+
 
 @Component({
   selector: 'app-album-detail',
   templateUrl: './album-detail.component.html',
-  styleUrls: ['./album-detail.component.css']
+  styleUrls: ['./album-detail.component.css'],
+  providers: [AlbumService]
 })
 export class AlbumDetailComponent implements OnInit {
 
-  albumId: number = null;
-
-  constructor(public route: ActivatedRoute, public location: Location) { }
+  albumId: number;
+  albumToDisplay: Album;
+  constructor(public route: ActivatedRoute, public location: Location, private albumService: AlbumService) { }
 
   ngOnInit() {
     this.route.params.forEach((urlParameters) => {
       this.albumId = parseInt(urlParameters['id']);
     })
+    this.albumToDisplay = this.albumService.getAlbumById(this.albumId);
   }
 
 }
